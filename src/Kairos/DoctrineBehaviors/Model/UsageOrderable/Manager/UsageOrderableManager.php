c033d971f5c859b7127beb46d64d17ddef464e0f
Alexandre Assouad <alexandre.assouad@gmail.com>
2014-08-18 11:24:18 +0200

    moved custom doctrine behaviors from knp to kairos

---
+++ src/Kairos/DoctrineBehaviors/Model/UsageOrderable/Manager/UsageOrderableManager.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Kairos\DoctrineBehaviors\Model\UsageOrderable\Manager;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Kairos\DoctrineBehaviors\Reflection\ClassAnalyzer;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Security\Core\SecurityContext;
+
+
+Class UsageOrderableManager
+{
+
+    /**
+     * @var EventDispatcherInterface
+     */
+    protected $dispatcher;
+
+    /**
+     * @var SecurityContext
+     */
+    protected $security;
+
+    /**
+     * @var EntityManager
+     */
+    protected $em;
+
+    /**
+     * @var ClassAnalyzer
+     */
+    protected $classAnalyzer;
+
+    /**
+     * @var bool
+     */
+    protected $isRecursive;
+
+    /**
+     * @var trait
+     */
+    private $usageOrderableTrait;
+
+    /**
+     * @var trait
+     */
+    private $usageTimestampTrait;
+
+    public function __construct(EventDispatcherInterface $dispatcher, SecurityContext $security, EntityManager $em, ClassAnalyzer $classAnalyzer, $isRecursive,
+                                $usageOrderableTrait, $usageTimestampTrait)
+    {
+        $this->dispatcher = $dispatcher;
+        $this->security = $security;
+        $this->em = $em;
+        $this->classAnalyzer = $classAnalyzer;
+        $this->isRecursive = (bool) $isRecursive;
+        $this->usageOrderableTrait = $usageOrderableTrait;
+        $this->usageTimestampTrait = $usageTimestampTrait;
+    }
+
+    public function addUsageTimestamp($entity) {
+
+        $entityMetadata = $this->em->getClassMetadata(get_class($entity));
+
+        if ($this->isUsageOrderable($entityMetadata)) {
+
+            $timestamp = $this->generateTimestamp($entity);
+            $timestampMetadata = $this->em->getClassMetadata(get_class($timestamp));
+
+            if ($this->isUsageTimestamp($timestampMetadata)) {
+
+                $userId = null;
+                if($user = $this->security->getToken()->getUser()) {
+                    if($user->getId()) {
+                        $userId = $user->getId();
+                    }
+                }
+
+                $entity->addUsageTimestamp($timestamp);
+                $timestamp->setUserId($userId)->setUsageOrderable($entity);
+                $this->save($entity);
+            }
+        }
+    }
+
+    /**
+     *
+     * @return UsageTimestamp
+     */
+    public function generateTimestamp($entity)
+    {
+        $timestamp = new \ReflectionClass($entity->getEntityName()."UsageTimestamp");
+        return $timestamp->newInstance();
+    }
+
+    /**
+     * @param $entity
+     */
+    public function save($entity)
+    {
+        $this->em->persist($entity);
+        $this->em->flush();
+    }
+
+
+    /**
+     * Checks if entity is UsageOrderable
+     *
+     * @param ClassMetadata $classMetadata
+     * @param bool          $isRecursive   true to check for parent classes until found
+     *
+     * @return boolean
+     */
+    private function isUsageOrderable(ClassMetadata $classMetadata, $isRecursive = false)
+    {
+        return $this->getClassAnalyzer()->hasTrait($classMetadata->reflClass, $this->usageOrderableTrait, $this->isRecursive);
+    }
+
+    /**
+     * @param  ClassMetadata $classMetadata
+     * @return boolean
+     */
+    private function isUsageTimestamp(ClassMetadata $classMetadata)
+    {
+        return $this->getClassAnalyzer()->hasTrait($classMetadata->reflClass, $this->usageTimestampTrait, $this->isRecursive);
+    }
+
+    /**
+     * @return ClassAnalyzer
+     */
+    public function getClassAnalyzer()
+    {
+        return $this->classAnalyzer;
+    }
+}
---
+++ src/Kairos/DoctrineBehaviors/Model/UsageOrderable/UsageOrderable.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Kairos\DoctrineBehaviors\Model\UsageOrderable;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Kairos\DoctrineBehaviors\Model as ORMBehaviors;
+use Doctrine\ORM\Event\LifeCycleEventArgs;
+
+trait UsageOrderable
+{
+
+    /**
+     * @var UsageTimestamp
+     */
+    protected $usageTimestamps;
+
+    /**
+     * Returns collection of translations.
+     *
+     * @return ArrayCollection
+     */
+    public function getUsageTimestamp()
+    {
+        return $this->usageTimestamps = $this->usageTimestamps ?: new ArrayCollection();
+    }
+
+    /**
+     * Adds new translation.
+     *
+     * @param UsageTimestamp $usageTimestamp
+     */
+    public function addUsageTimestamp($usageTimestamp)
+    {
+        $this->getUsageTimestamp()->add($usageTimestamp);
+        $usageTimestamp->setUsageOrderable($this);
+    }
+
+    /**
+     * Removes specific translation.
+     *
+     * @param UsageTimestamp $usageTimestamp
+     */
+    public function removeUsageTimestamp($usageTimestamp)
+    {
+        $this->getUsageTimestamp()->removeElement($usageTimestamp);
+    }
+
+    /**
+     *
+     * @return UsageOrderable
+     */
+    public function incrementUsageTimestamp()
+    {
+        $timestamp = $this->generateTimestamp();
+        $this->addUsageTimestamp($timestamp);
+        $timestamp->setUsageOrderable($this);
+    }
+
+    /**
+     * Returns entity class name.
+     *
+     * @return string
+     */
+    public static function getEntityName()
+    {
+        return  __CLASS__;
+    }
+}
